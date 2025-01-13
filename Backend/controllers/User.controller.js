71da0a69b69b3ae3579304c49eeb451bb7e3fc9e
VinaySingh1045 <vy0891547@gmail.com>
2025-01-13 17:48:25 +0530

    signup controller updated

--- Backend/controllers/User.controller.js
+++ Backend/controllers/User.controller.js
@@ -28,46 +28,52 @@ const generateAccessAndRefreshToken = async (userId) => {
 
 
 const userRegister = AsyncHandler(async (req, res) => {
-    const { fullName, email, password } = req.body
-
-
-    // Validation
-    if (!fullName || !email || !password) {
-        throw new ApiError(400, "All fileds are required")
+    try {
+        
+        const { fullName, email, password } = req.body
+    
+    
+        // Validation
+        if (!fullName || !email || !password) {
+            throw new ApiError(400, "All fileds are required")
+        }
+    
+        // Check if the email is already exists
+        const existsUser = await User.findOne({ email })
+    
+        if (existsUser) {
+            throw new ApiError(409, "Email already exists")
+        }
+    
+        // Now uploading the file to the cloudinary service
+    
+        let avatarFilePath;
+    
+        if (req.file) {
+            avatarFilePath = req.file.path
+        }
+        else {
+            console.log("Something went wrong");
+        }
+    
+        const avatar = await uploadOnCloudinary(avatarFilePath);
+    
+        // Create the user
+        const user = await User.create({
+            fullName,
+            email,
+            password,
+            avatar: avatar?.url || "",
+        })
+    
+        return res.status(201).json(
+            new ApiResponse(200, user, "User Register Successfully")
+        )
+    
+    } catch (error) {
+        console.error('Error in userRegister:', error);
+        throw new ApiError(400, "Something went wrong while registering the user")
     }
-
-    // Check if the email is already exists
-    const existsUser = await User.findOne({ email })
-
-    if (existsUser) {
-        throw new ApiError(409, "Email already exists")
-    }
-
-    // Now uploading the file to the cloudinary service
-
-    let avatarFilePath;
-
-    if (req.file) {
-        avatarFilePath = req.file.path
-    }
-    else {
-        console.log("Something went wrong");
-    }
-
-    const avatar = await uploadOnCloudinary(avatarFilePath);
-
-    // Create the user
-    const user = await User.create({
-        fullName,
-        email,
-        password,
-        avatar: avatar?.url || "",
-    })
-
-    return res.status(201).json(
-        new ApiResponse(200, user, "User Register Successfully")
-    )
-
 })
 
 const userLogin = AsyncHandler(async (req, res) => {
